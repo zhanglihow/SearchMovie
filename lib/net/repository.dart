a759642562c78e44c5676d625e6ee145a0e031e7
邓文浩 <aboutmydreams@163.com>
2019-05-03 11:50:24 +0800

    fix a bug

--- lib/net/repository.dart
+++ lib/net/repository.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:search_movie/data/movie.dart';
 import 'package:http/http.dart' as http;
 import 'package:search_movie/data/movie_detail.dart';
-
 
 class ParsedResponse<T> {
   ParsedResponse(this.statusCode, this.body);
@@ -19,12 +18,10 @@ class ParsedResponse<T> {
 
 final int NO_INTERNET = 404;
 
-
 class Repository {
-
   static Future<ParsedResponse<List<Movie>>> getMovies(String input) async {
-    http.Response response = await new http.Client().get(
-        "https://api.douban.com/v2/movie/search?q=$input");
+    http.Response response = await new http.Client()
+        .get("https://api.douban.com/v2/movie/search?q=$input");
     print("response:" + response.toString());
 
     if (response == null) {
@@ -34,7 +31,7 @@ class Repository {
     if (response.statusCode < 200 || response.statusCode >= 300) {
       return new ParsedResponse(response.statusCode, []);
     }
-    List<dynamic> list = JSON.decode(response.body)['subjects'];
+    List<dynamic> list = jsonDecode(response.body)['subjects'];
 
     Map<String, Movie> networkMovies = {};
 
@@ -47,10 +44,9 @@ class Repository {
         response.statusCode, []..addAll(networkMovies.values));
   }
 
-
-  static Future<ParsedResponse<MovieDetail>> getMovieDetails(String id) async{
-    http.Response response = await new http.Client().get(
-        "https://api.douban.com/v2/movie/subject/"+id);
+  static Future<ParsedResponse<MovieDetail>> getMovieDetails(String id) async {
+    http.Response response = await new http.Client()
+        .get("https://api.douban.com/v2/movie/subject/" + id);
 
     if (response == null) {
       return new ParsedResponse(NO_INTERNET, null);
@@ -58,9 +54,8 @@ class Repository {
     if (response.statusCode < 200 || response.statusCode >= 300) {
       return new ParsedResponse(response.statusCode, null);
     }
-    dynamic details =JSON.decode(response.body);
-    MovieDetail movieDetails=MovieDetail.formJson(details);
-    return new ParsedResponse( response.statusCode,movieDetails);
+    dynamic details = jsonDecode(response.body);
+    MovieDetail movieDetails = MovieDetail.formJson(details);
+    return new ParsedResponse(response.statusCode, movieDetails);
   }
-
 }
